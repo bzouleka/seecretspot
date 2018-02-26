3b1064b2df2071b5bc18ce4c09754467904ea74e
bzouleka <bzouleka@gmail.com>
2018-02-26 14:02:04 +0100

    table friends

---
+++ database/migrations/2018_02_26_125659_create_friends_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFriendsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friends', function (Blueprint $table) {
+            $table->increments('id_user1');
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->increments('id_user2');
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friends');
+    }
+}
