0dbf7daa320e1b29ec64061c74a24fb49f18e3a4
darky74 <julien.fourdachon@gmail.com>
2018-02-26 16:03:43 +0100

    moitié des routes faites

--- app/Http/Controllers/SignUpController.php
+++ app/Http/Controllers/SignUpController.php
@@ -5,30 +5,30 @@ namespace App\Http\Controllers;
 
 
 use Illuminate\Http\Request;
-
+use App\User;
 
 class LoginController extends Controller
 {
     public function create()
     {
-        return view('inscription');
+        return view('signUp');
     }
 
 
 
     public function store (Request $request)
     {
-        /*$user = new User();
+        $user = new User();
         $user->email = $request['email'];
         $user->first_name = $request['first_name'];
         $user->last_name = $request['last_name'];
         $user->user_name = $request['user_name'];
         $user->country = $request['country'];
         $user->password = $request['password'];
-        $user->confirm_password = $request['confirm_password'];
 
-        $user->save();*/
 
-        return redirect()->route('index');
+        $user->save();
+
+        return redirect()->route('login');
     }
 }
