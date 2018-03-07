05f16fe4435725170e7c3dbff8207b967344e7c5
darky74 <julien.fourdachon@gmail.com>
2018-03-07 16:46:01 +0100

    connexion utilisateur inscrit uniquement

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -14,7 +14,18 @@ class LoginController extends Controller
 
     public function store()
     {
-        return view('homePage');
+
+        $Result = auth()->attempt([
+            'user_name' => request('user_name'),
+            'password' => request('password'),
+        ]);
+
+        if($Result) {
+            return view('homePage');
+        }
+        return back()->withErrors([
+            'user_name' => "Vos identifiants sont incorrects."
+        ]);
     }
 
 }
--- app/User.php
+++ app/User.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Auth\Authenticatable as BasicAuthenticatable;
 
-class User extends Model
+class User extends Model implements Authenticatable
 {
-    //
+    use BasicAuthenticatable;
+
+    protected $fillable = ['user_name', 'password'];
+
+
 }
