038858b48a2a22980aa99948bcd3214fc1e8ebe4
darky74 <julien.fourdachon@gmail.com>
2018-03-07 14:32:58 +0100

    commit de rien du tut

--- app/Http/Controllers/SignUpController.php
+++ app/Http/Controllers/SignUpController.php
@@ -19,7 +19,7 @@ class SignUpController extends Controller
 
 
 
-    public function store (Request $request)
+    public function store (UserRequest $request)
     {
         $user = new User();
         $user->email = $request['email'];
