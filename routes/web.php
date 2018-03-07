81ef0a13f2f4b9b51b1a848a7309baba03bfb08c
bzouleka <bzouleka@gmail.com>
2018-03-07 14:29:18 +0100

    ...

--- routes/web.php
+++ routes/web.php
@@ -26,13 +26,10 @@ Route::get('/myProfile', 'MyProfileController@create')->name('profileUser');
 Route::post('/myProfile', 'MyProfileController@post')->name('profileUserPost');
 
 Route::get('/', 'SignUpController@store')->name('login');
-
 Route::post('/', 'SignUpController@store')->name('login');
-
 Route::view('/', 'login');
 
 Route::get('/HomePage',['uses' => 'HomePageController@getHomePage', 'as' => 'getHomePage']);
-
 Route::post('/HomePage',['uses' => 'HomePageController@getHomePage', 'as' => 'HomePage']);
 
 Route::get('/Message', 'MessageController@create')->name('message');
@@ -42,5 +39,4 @@ Route::get('/Message', 'MessageController@create')->name('message');
 
 
 Route::get('/spot', ['uses' => 'SpotController@getNewSpot', 'as' => 'newSpot']);
-
 Route::post('/spot',['uses' => 'SpotController@storeSpot', 'as' => 'newPost']);
