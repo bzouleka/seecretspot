3cb2bf8f4fd5c4e3c0227e3fd646cdc02564998f
darky74 <julien.fourdachon@gmail.com>
2018-03-08 13:58:27 +0100

    update table users via le formulaires settings

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 
 class SettingsController extends Controller
@@ -15,11 +15,32 @@ class SettingsController extends Controller
     }
 
 
-    public function update(Request $request)
+    public function update(SettingsRequest $request)
     {
+        $user = Auth::user();
+        if(isset($request['last_name']) && !empty($request['last_name'])) {
+            $user->last_name = $request['last_name'];
+        }
+        if(isset($request['first_name']) && !empty($request['first_name'])) {
+            $user->first_name = $request['first_name'];
+        }
+        if(isset($request['birthday']) && !empty($request['birthday'])) {
+            $user->birthday = $request['birthday'];
+        }
+        if(isset($request['user_name']) && !empty($request['user_name'])) {
+            $user->user_name = $request['user_name'];
+        }
+        if(isset($request['email']) && !empty($request['email'])) {
+            $user->email = $request['email'];
+        }
+        if(isset($request['description']) && !empty($request['description'])) {
+            $user->description = $request['description'];
+        }
+        if(isset($request['password']) && !empty($request['password'])) {
+            $user->password = $request['password'];
+        }
+        $user->update();
 
-       /* $settings = new Settings();*/
-
-
+        return redirect()->route('myProfile');
     }
 }
