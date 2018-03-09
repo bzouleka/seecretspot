08b86b7f8e3f75b61ccd9b72dcd409cea7c3cde9
isagri <isabelle.grillet6@orange.fr>
2018-03-09 14:53:22 +0100

    Homepage et myprofile : affichage des amis

--- resources/views/myProfile.blade.php
+++ resources/views/myProfile.blade.php
@@ -20,6 +20,16 @@
                     </form>
                 </div>           
               </div> 
+
+            @foreach ($friends as $friend)
+              <div class="row">
+                <div class="col-md-6">
+                    <br/>
+                    <img src="{{ asset($friend->picture_name) }} " alt="{{ $friend->user_name }}" width="100%" />
+                </div>
+                <div>{{ $friend->user_name }}</div>
+              </div>
+            @endforeach
 <!--                <div><br/>Contactez moi pour partager<br/>#surf #glisse #montagne</div>   -->
             </div>
 
@@ -44,9 +54,10 @@
   -->
             <div class="col-md-8">
 
-                @foreach ($spots as $spot)
-                <img src="{{ asset($spot->picture_name) }}" 
-                     alt="{{ asset($spot->title) }}" width="100%" />
+              @foreach ($spots as $spot)
+                <a href="{{ '/spot?id='.$spot->id }}">
+                    <img src="{{ asset($spot->picture_name) }}" alt="{{ asset($spot->title) }}" width="100%" />
+                </a>
                 <div class="row">
                     <div class="col-xs-6">
                         <p>{{ $spot->title }}</p>
@@ -62,7 +73,7 @@
                         <p>{{ $spot->description_post }}</p>
                     </div>
                 </div>
-                @endforeach
+              @endforeach
             </div>
         </section>
     </div>
